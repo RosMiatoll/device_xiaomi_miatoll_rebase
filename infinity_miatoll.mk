45337531ecfd139d700c854af939b742bb92de77
Afif Momin <98091583+Afifmomin@users.noreply.github.com>
2025-07-30 21:59:40 +0530

    miatoll: InfinityX bringup

--- infinity_miatoll.mk
+++ infinity_miatoll.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 # Inherit from miatoll device
 $(call inherit-product, device/xiaomi/miatoll/atoll.mk)
@@ -19,7 +19,13 @@ $(call inherit-product, device/xiaomi/miatoll/atoll.mk)
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
-PRODUCT_NAME := lineage_miatoll
+# Infinity Flags
+INFINITY_BUILD_TYPE := OFFICIAL
+INFINITY_MAINTAINER := AFIF
+TARGET_SUPPORTS_BLUR := true
+WITH_GAPPS := true
+
+PRODUCT_NAME := infinity_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
