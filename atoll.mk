b872169ed45d5808b7e2e81475b81d30bf31e6d0
AbdoAhmedElbanaa <115952879+AbdoAhmedElbanaa@users.noreply.github.com>
2025-11-27 19:55:24 +0200

    Fix formatting in atoll.mk for camera provider

--- atoll.mk
+++ atoll.mk
@@ -50,7 +50,7 @@ $(call soong_config_set,camera,override_format_from_reserved,true)
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64 \
+    android.hardware.camera.provider@2.4-service_64 
 
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
